df8f2f7d18ad93eadca8bfd97913b97c3eb6e0e9
amctech106 <121874402+amctech106@users.noreply.github.com>
2025-08-12 11:46:07 +0500

    Functions, Argument & parameters
    Functions, Argument & parameters, Default & rest Operators.

---
+++ Types of Function/script.js
@@ -0,0 +1,73 @@
+///1....function declearation or function statement -----------------
+
+/* function abc (){
+    console.log("Aslaam o Alaikum ");
+}
+
+abc();
+ */
+
+///2...function expression -----------------------------------------
+//ye eo function he jo hamesha variable k sath hota he. 
+
+/* let xyz = function(){
+    console.log("Wa Alaikum Salaam");
+
+}
+
+xyz();
+ */
+
+//3....Fat Arrow Function .---------------------------------------
+
+/* let zaid =()=>{
+console.log("how are you ?");
+}
+
+zaid(); */
+
+//4....Argument and Parameters-----------------------------------
+/* function abc (v1, v2){
+    console.log(v1 , v2); 
+}
+
+abc(12, 13); */
+
+//Argument and Parameters with dynamic value example---------------
+
+/* function abc(v1){
+console.log(`${v1} is Talking`);
+}
+
+abc("Asad"); */
+
+
+//Defaul Parameters Example------------------------------------------
+// is ka result undefined undefined hoga 
+
+/* function abc(v1, v2){
+    console.log(v1, v2);
+
+}
+
+abc(); */
+
+//Defaul Parameters Example 2...--------------------------------------
+// is ka answer zero zero hoga. 
+
+/* function abc(v1=0, v2=0){
+    console.log(v1, v2);
+
+}
+
+abc(); */
+
+
+//rest Operators  Example ...-----------------------------------------
+/* function abc(v, ...v1){
+    console.log(v,v1);
+
+}
+
+abc(1,2,3,4,6,7,8,9,10); */
+
